f2c99be28c9c999bca7652dd4ceee27a5ce10d67
zubair-source <zubairsafi103@gmail.com>
2023-05-06 19:14:34 +0500

    Comiler

---
+++ ast.tab.h
@@ -0,0 +1,109 @@
+/* A Bison parser, made by GNU Bison 3.5.1.  */
+
+/* Bison interface for Yacc-like parsers in C
+
+   Copyright (C) 1984, 1989-1990, 2000-2015, 2018-2020 Free Software Foundation,
+   Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+/* Undocumented macros, especially those whose name start with YY_,
+   are private implementation details.  Do not rely on them.  */
+
+#ifndef YY_YY_AST_TAB_H_INCLUDED
+# define YY_YY_AST_TAB_H_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
+
+/* Token type.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+  enum yytokentype
+  {
+    HASH = 258,
+    INCLUDE = 259,
+    IOSTREAM = 260,
+    STRING_LITERAL = 261,
+    HEADER_LITERAL = 262,
+    PRINT = 263,
+    RETURN = 264,
+    INTEGER_LITERAL = 265,
+    CHARACTER_LITERAL = 266,
+    FLOAT_LITERAL = 267,
+    IDENTIFIER = 268,
+    INC_OP = 269,
+    DEC_OP = 270,
+    LE_OP = 271,
+    GE_OP = 272,
+    EQ_OP = 273,
+    NE_OP = 274,
+    MUL_ASSIGN = 275,
+    DIV_ASSIGN = 276,
+    MOD_ASSIGN = 277,
+    ADD_ASSIGN = 278,
+    SUB_ASSIGN = 279,
+    CHAR = 280,
+    INT = 281,
+    FLOAT = 282,
+    VOID = 283,
+    FOR = 284,
+    WHILE = 285,
+    IF = 286
+  };
+#endif
+
+/* Value type.  */
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+union YYSTYPE
+{
+#line 92 "ast.y"
+
+	int ival;
+	float fval;
+	char cval;
+	char string[128];
+	struct node *ptr;
+
+#line 97 "ast.tab.h"
+
+};
+typedef union YYSTYPE YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+
+extern YYSTYPE yylval;
+
+int yyparse (void);
+
+#endif /* !YY_YY_AST_TAB_H_INCLUDED  */
---
+++ input.c
@@ -0,0 +1,10 @@
+int main(){
+	
+	int i, a, b;
+	int nume=3.45;	
+	for(i = 0;  i < 10; i++){
+		a=i;
+	}
+	i=1;
+}
+
